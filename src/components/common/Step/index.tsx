4e5178d2a469733df615a66a42c4598133a2557a
Curtis <curtis_102@hotmail.com>
2024-02-25 23:10:05 -0800

    added steps

--- src/components/common/Step/index.tsx
+++ src/components/common/Step/index.tsx
@@ -9,11 +9,22 @@ interface StepProps {
 
 const Step: React.FC<StepProps> = ({ number, title, description }) => {
   return (
-    <div className='step'>
-      <div className='step-number'>{number}</div>
-      <h3 className='step-title'>{title}</h3>
-      <p className='step-description'>{description}</p>
-    </div>
+    <>
+      <div className='step mobile'>
+        <div className='step-header'>
+          <div className='step-number'>{number}</div>
+          <h3 className='step-title'>{title}</h3>
+        </div>
+        <p className='step-description'>{description}</p>
+      </div>
+      <div className='step'>
+        <div className='step-number'>{number}</div>
+        <div className='step-header'>
+          <h3 className='step-title'>{title}</h3>
+          <p className='step-description'>{description}</p>
+        </div>
+      </div>
+    </>
   );
 };
 
