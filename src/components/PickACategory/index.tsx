f75ce065abc9faf8226cfc924130b24bf970af22
Curtis <curtis_102@hotmail.com>
2024-02-27 01:08:35 -0800

    added more categories

--- src/components/PickACategory/index.tsx
+++ src/components/PickACategory/index.tsx
@@ -11,11 +11,14 @@ const PickACategory: React.FC<PickACategoryProps> = ({ goBack }) => {
   return (
     <div className='pick-a-category'>
       <Header goBack={goBack} headerTxt='Pick a Category' />
-      {/* <div className='how-to-play-container'>
-        <Step number='01' title='CHOOSE A CATEGORY' description='First, choose a word category, like animals or movies. The computer then randomly selects a secret word from that topic and shows you blanks for each letter of the word.' />
-        <Step number='02' title='GUESS LETTERS' description="Take turns guessing letters. The computer fills in the relevant blank spaces if your guess is correct. If it's wrong, you lose some health, which empties after eight incorrect guesses." />
-        <Step number='03' title='WIN OR LOSE' description='You win by guessing all the letters in the word before your health runs out. If the health bar empties before you guess the word, you lose.' />
-      </div> */}
+      <div className='pick-a-category-container'>
+        <div className='item'>MOVIES</div>
+        <div className='item'>TV SHOWS</div>
+        <div className='item'>COUNTRIES</div>
+        <div className='item'>CAPITAL CITIES</div>
+        <div className='item'>ANIMALS</div>
+        <div className='item'>SPORTS</div>
+      </div>
     </div>
   );
 };
